fb7d65fc92736b5ccf210e460269dc51cc9d628d
Felipe Cesar <felipe.cesar@totvs.com.br>
2021-06-01 20:53:58 -0300

    Projeto Base Atualizado

--- main.py
+++ main.py
@@ -3,40 +3,43 @@ def maiorNumero(n1, n2):
     maiorNumero = [n1, n2]
     return max(maiorNumero)
 
+def soma(n1, n2):
+    return n1 + n2
+
+def subtracao(n1, n2):
+    largest_number = maiorNumero(n1, n2)
+    
+    if(largest_number == n1):
+        return n1 - n2
+    
+    if(largest_number == n2):
+        return n2 - n1
+
+def multiplicacao(n1, n2):
+    return n1 * n2
+
+def divisao(n1, n2):
+    largest_number = maiorNumero(n1 ,n2)
+
+    if(largest_number == n1):
+        return n1 / n2
+    
+    if(largest_number == n2):
+        return n2 / n1
+
 def calcular(operacao, n1, n2):
 
     if(operacao == '+'):
-        return n1 + n2
+        soma(n1 ,n2)
 
     if(operacao == '-'):
-        largest_number = maiorNumero(n1, n2)
-        
-        if(largest_number == n1):
-            return n1 - n2
-        
-        if(largest_number == n2):
-            return n2 - n1
+        subtracao(n1, n2)
     
     if(operacao == '*'):
-        return n1 * n2
+        multiplicacao(n1, n2)
 
     if(operacao == '/'):
-        largest_number = maiorNumero(n1 ,n2)
-
-        if(largest_number == n1):
-            return n1 / n2
-        
-        if(largest_number == n2):
-            return n2 / n1
-
-    if(operacao == '=-'):
-        largest_number = maiorNumero(n1 ,n2)
-
-        if(largest_number == n1):
-            return n1 / n2
-        
-        if(largest_number == n2):
-            return n2 / n1
+        divisao(n1, n2)
 
 def calculadora():
 
